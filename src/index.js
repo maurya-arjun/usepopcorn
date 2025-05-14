79990875fe0834210901e957772936d21f843e8e
ARJUN KUMAR MARUYA <arjunkumarmaruya@ARJUNs-MacBook-Air.local>
2025-05-14 07:37:57 +0530

    make rating component more flexible and reusable using more props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        size={30}
+        color="blue"
+        onSetRating={setMovieRating}
+      />
+      <p>The movie has already been reated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terriable", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={5} size={25} color="red" defaultRating={2} />
+
+    <Test />
   </React.StrictMode>
 );
