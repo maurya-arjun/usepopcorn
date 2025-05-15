7c1b8cfee83dfc0edbdecbcdea1ed31540ff602d
ARJUN KUMAR MARUYA <arjunkumarmaruya@ARJUNs-MacBook-Air.local>
2025-05-15 06:02:17 +0530

    create text expander resuable componenet

---
+++ src/TextExpander.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const TextExpander = ({
+  expandButtonText = "Show more",
+  collapsedButtonText = "Hide more",
+  buttonTextColor = "red",
+  expanded = false,
+  collapsedNumWords = 10,
+  children,
+  className = "",
+}) => {
+  const [isExpand, setIsExpand] = useState(expanded);
+
+  const displayText = isExpand
+    ? children
+    : children.split(" ").slice(0, collapsedNumWords).join(" ") + "...";
+
+  const buttonStyle = {
+    background: "none",
+    font: "inherit",
+    border: "none",
+    color: buttonTextColor,
+  };
+
+  return (
+    <div className={className}>
+      <span>{displayText}</span>{" "}
+      <button onClick={() => setIsExpand((open) => !open)} style={buttonStyle}>
+        {isExpand ? collapsedButtonText : expandButtonText}
+      </button>
+    </div>
+  );
+};
+
+export default TextExpander;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,60 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
+import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+import TextExpander from "./TextExpander";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating
-        maxRating={10}
-        size={30}
-        color="blue"
-        onSetRating={setMovieRating}
-      />
-      <p>The movie has already been reated {movieRating} stars</p>
-    </div>
-  );
-}
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating
+//         maxRating={10}
+//         size={30}
+//         color="blue"
+//         onSetRating={setMovieRating}
+//       />
+//       <p>The movie has already been reated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating
-      maxRating={5}
-      messages={["Terriable", "Bad", "Okay", "Good", "Amazing"]}
-    />
-    <StarRating maxRating={5} size={25} color="red" defaultRating={2} />
+    <StarRating maxRating={5} size={26} color="red" />
+    {/* <StarRating maxRating={5} size={25} color="red" defaultRating={2} /> */}
 
-    <Test />
+    {/* <Test /> */}
+    <TextExpander expanded={true}>
+      It is a long established fact that a reader will be distracted by the
+      readable content of a page when looking at its layout. The point of using
+      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+      as opposed to using 'Content here, content here', making it look like
+      readable English. Many desktop publishing packages and web page editors
+      now use Lorem Ipsum as their default model text, and a search for 'lorem
+      ipsum' will uncover many web sites still in their infancy. Various
+      versions have evolved over the years, sometimes by accident, sometimes on
+      purpose (injected humour and the like)
+    </TextExpander>
+    <TextExpander
+      expandButtonText="Text more"
+      buttonTextColor="green"
+      collapsedButtonText="Text hide"
+      collapsedNumWords={75}
+      className="box"
+    >
+      It is a long established fact that a reader will be distracted by the
+      readable content of a page when looking at its layout. The point of using
+      Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+      as opposed to using 'Content here, content here', making it look like
+      readable English. Many desktop publishing packages and web page editors
+      now use Lorem Ipsum as their default model text, and a search for 'lorem
+      ipsum' will uncover many web sites still in their infancy. Various
+      versions have evolved over the years, sometimes by accident, sometimes on
+      purpose (injected humour and the like)
+    </TextExpander>
   </React.StrictMode>
 );
